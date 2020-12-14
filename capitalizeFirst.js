c138b3f3dcdcdf8d44b9a6d952ea49c5369b0239
thomas-ode <thomas.ode.w@gmail.com>
2020-12-14 09:42:33 +0100

    test and implementation

---
+++ capitalizeFirst.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+
+function capitalizeFirst(input) {
+
+    return input.length > 0
+  
+      ? input[0].toUpperCase() + input.slice(1)
+  
+      : '';
+  
+  }
+
+// Check that capitalizeFirst is a function
+
+assert.strictEqual(typeof capitalizeFirst, 'function');
+
+
+// Check that capitalizeFirst accepts one argument
+
+assert.strictEqual(capitalizeFirst.length, 1);
+
+
+// Check that capitalizeFirst transforms javaScript correctly
+
+assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+
+
+// Check that it works for a 1-character string
+
+assert.strictEqual(capitalizeFirst('z'), 'Z');
+
+
+// Check that it works for an empty string
+
+assert.strictEqual(capitalizeFirst(''), '');
